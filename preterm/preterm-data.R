2a3e740707642ca725ea1d361c4d53b4ddc9a73e
c-voulgaris <cvoulgaris@gsd.harvard.edu>
2023-08-15 14:52:48 -0400

    Added data for preterm workshop

---
+++ preterm/preterm-data.R
@@ -0,0 +1,29 @@
+library(tidycensus)
+library(tidyverse)
+library(here)
+
+vars <- c(med_age_ = "B01002_001",
+          med_inc_ = "B21004_001")
+
+zips <- get_acs(geography = "zcta", 
+                     variables = vars, year = 2011, 
+                     output = "wide") |>
+  mutate(year = 2011)
+
+for (i in 2012:2021) {
+  this_zips <- get_acs(geography = "zcta", 
+                       variables = vars, year = i, 
+                       output = "wide") |>
+    mutate(year = i)
+  
+  zips <- rbind(zips, this_zips)
+}
+
+zips_messy <- zips |>
+  mutate(age_income = paste0("Age: ", med_age_E, " / Income: ", med_inc_E)) |>
+  mutate(ZIP = substr(NAME, 7, 11)) |>
+  select(ZIP, year, age_income) |>
+  pivot_wider(names_from = year, values_from = age_income)
+
+write_csv(zips_messy, here("preterm",
+                           "messy_data.csv"))
